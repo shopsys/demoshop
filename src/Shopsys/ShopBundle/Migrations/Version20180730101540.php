bfd59f32d3f841cfdf4ba264e2123b76b018bb7e
Miroslav Stopka <stopka.miroslav.sk@gmail.com>
2018-07-31 14:02:47 +0200

    Category: added second description attribute

---
+++ src/Shopsys/ShopBundle/Migrations/Version20180730101540.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Shopsys\ShopBundle\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Shopsys\MigrationBundle\Component\Doctrine\Migrations\AbstractMigration;
+
+class Version20180730101540 extends AbstractMigration
+{
+    /**
+     * @param \Doctrine\DBAL\Schema\Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $this->sql('ALTER TABLE category_domains ADD description_second TEXT DEFAULT NULL');
+    }
+
+    /**
+     * @param \Doctrine\DBAL\Schema\Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+    }
+}
---
+++ src/Shopsys/ShopBundle/Model/Category/CategoryDomain.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Shopsys\ShopBundle\Model\Category;
+
+use Doctrine\ORM\Mapping as ORM;
+use Shopsys\FrameworkBundle\Model\Category\CategoryDomain as BaseCategoryDomain;
+
+/**
+ * @ORM\Table(
+ *     name="category_domains",
+ *     uniqueConstraints={
+ *         @ORM\UniqueConstraint(name="category_domain", columns={"category_id", "domain_id"})
+ *     }
+ * )
+ * @ORM\Entity
+ */
+class CategoryDomain extends BaseCategoryDomain
+{
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(type="text", nullable=true)
+     */
+    protected $descriptionSecond;
+
+    /**
+     * @return string|null
+     */
+    public function getDescriptionSecond()
+    {
+        return $this->descriptionSecond;
+    }
+
+    /**
+     * @param string $descriptionSecond
+     */
+    public function setDescriptionSecond($descriptionSecond)
+    {
+        $this->descriptionSecond = $descriptionSecond;
+    }
+}
