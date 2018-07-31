03a30c0d87a479db89a692f857814e300854fac3
Miroslav Stopka <stopka.miroslav.sk@gmail.com>
2018-07-31 15:19:03 +0200

    GoogleFeedItemFactory: removed unused imports

--- src/Shopsys/ShopBundle/Model/Product/Feed/Google/GoogleFeedItemFactory.php
+++ src/Shopsys/ShopBundle/Model/Product/Feed/Google/GoogleFeedItemFactory.php
@@ -3,9 +3,6 @@
 namespace Shopsys\ShopBundle\Model\Product\Feed\Google;
 
 use Shopsys\FrameworkBundle\Component\Domain\Config\DomainConfig;
-use Shopsys\FrameworkBundle\Model\Pricing\Currency\CurrencyFacade;
-use Shopsys\FrameworkBundle\Model\Product\Collection\ProductUrlsBatchLoader;
-use Shopsys\FrameworkBundle\Model\Product\Pricing\ProductPriceCalculationForUser;
 use Shopsys\FrameworkBundle\Model\Product\Product as BaseProduct;
 use Shopsys\ProductFeed\GoogleBundle\Model\FeedItem\GoogleFeedItem as BaseGoogleFeedItem;
 use Shopsys\ProductFeed\GoogleBundle\Model\FeedItem\GoogleFeedItemFactory as BaseGoogleFeedItemFactory;
