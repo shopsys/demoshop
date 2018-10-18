60e5db64ba3fbc834cae39770bdec016fbbe7380
Rostislav Vitek <rostislav.vitek@shopsys.com>
2018-10-18 14:39:36 +0200

    refactoring

--- src/Shopsys/ShopBundle/Model/Article/ArticleDataFactory.php
+++ src/Shopsys/ShopBundle/Model/Article/ArticleDataFactory.php
@@ -9,29 +9,29 @@ use Shopsys\FrameworkBundle\Component\Router\FriendlyUrl\FriendlyUrlFacade;
 use Shopsys\FrameworkBundle\Model\Article\Article as BaseArticle;
 use Shopsys\FrameworkBundle\Model\Article\ArticleData as BaseArticleData;
 use Shopsys\FrameworkBundle\Model\Article\ArticleDataFactory as BaseArticleDataFactory;
-use Shopsys\ShopBundle\Model\Article\ArticleProduct\ArticleProductRepository;
 
 class ArticleDataFactory extends BaseArticleDataFactory
 {
+
     /**
-     * @var \Shopsys\ShopBundle\Model\Article\ArticleProduct\ArticleProductRepository
+     * @var \Shopsys\ShopBundle\Model\Article\ArticleFacade
      */
-    private $articleProductRepository;
+    private $articleFacade;
 
     /**
      * @param \Shopsys\FrameworkBundle\Component\Router\FriendlyUrl\FriendlyUrlFacade $friendlyUrlFacade
      * @param \Shopsys\FrameworkBundle\Component\Domain\Domain $domain
      * @param \Shopsys\FrameworkBundle\Component\Domain\AdminDomainTabsFacade $adminDomainTabsFacade
-     * @param \Shopsys\ShopBundle\Model\Article\ArticleProduct\ArticleProductRepository $articleProductRepository
+     * @param \Shopsys\ShopBundle\Model\Article\ArticleFacade $articleFacade
      */
     public function __construct(
         FriendlyUrlFacade $friendlyUrlFacade,
         Domain $domain,
         AdminDomainTabsFacade $adminDomainTabsFacade,
-        ArticleProductRepository $articleProductRepository
+        ArticleFacade $articleFacade
     ){
-        $this->articleProductRepository = $articleProductRepository;
         parent::__construct($friendlyUrlFacade, $domain, $adminDomainTabsFacade);
+        $this->articleFacade = $articleFacade;
     }
 
     /**
@@ -44,7 +44,7 @@ class ArticleDataFactory extends BaseArticleDataFactory
         $this->fillFromArticle($articleData, $article);
 
         $articleData->createdAt = $article->getCreatedAt() ?? new DateTime();
-        $articleData->products = $this->getProductsByArticle($article);
+        $articleData->products = $this->articleFacade->getProductsByArticle($article);
 
         return $articleData;
     }
@@ -58,21 +58,5 @@ class ArticleDataFactory extends BaseArticleDataFactory
         $this->fillNew($articleData);
 
         return $articleData;
-    }
-
-    /**
-     * @param \Shopsys\ShopBundle\Model\Article\Article $article
-     * @return \Shopsys\ShopBundle\Model\Product\Product[]
-     */
-    private function getProductsByArticle(Article $article)
-    {
-        $articleProducts = $this->articleProductRepository->getArticleProductsByArticle($article);
-
-        $products = [];
-        foreach ($articleProducts as $articleProduct) {
-            $products[] = $articleProduct->getProduct();
-        }
-
-        return $products;
     }
 }
