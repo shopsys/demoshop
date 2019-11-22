78e3afcaf452bab68a2cee1aed571a553513113a
David Stritezsky <david.stritezsky@shopsys.com>
2019-11-22 17:51:40 +0100

    fixed forgotten comment

--- src/Shopsys/ShopBundle/Resources/scripts/frontend/product/productList.AjaxFilter.js
+++ src/Shopsys/ShopBundle/Resources/scripts/frontend/product/productList.AjaxFilter.js
@@ -63,7 +63,7 @@
             var $productsHtml = $wrappedData.find('.js-product-list-ajax-filter-products-with-controls');
             $productsWithControls.html($productsHtml.html());
             $productsWithControls.show();
-            //Shopsys.lazyLoadCall.inContainer($productsWithControls);
+            Shopsys.lazyLoadCall.inContainer($productsWithControls);
             Shopsys.register.registerNewContent($productsWithControls);
         };
 
