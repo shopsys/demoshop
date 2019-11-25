4e0c9e4e6eb322daf83ed389877dded715ad8230
David Stritezsky <david.stritezsky@shopsys.com>
2019-11-25 10:35:42 +0100

    added error identifier when response is 500

--- tests/ShopBundle/Acceptance/acceptance/ErrorHandlingCest.php
+++ tests/ShopBundle/Acceptance/acceptance/ErrorHandlingCest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Tests\ShopBundle\Acceptance\acceptance;
 
+use PHPUnit\Framework\Assert;
 use Tests\ShopBundle\Test\Codeception\AcceptanceTester;
 
 class ErrorHandlingCest
@@ -17,5 +18,20 @@ class ErrorHandlingCest
         $me->amOnPage('/test/error-handler/notice');
         $me->see('Oops! Error occurred');
         $me->dontSee('Notice');
+    }
+
+    /**
+    +     * @param \Tests\ShopBundle\Test\Codeception\AcceptanceTester $me
+    +     */
+    public function test500ErrorPage(AcceptanceTester $me)
+    {
+        $me->wantTo('display 500 error and check error ID uniqueness');
+        $me->amOnPage('/test/error-handler/exception');
+        $me->see('Oops! Error occurred');
+        $cssIdentifier = ['css' => '#js-error-id'];
+        $errorIdFirstAccess = $me->grabTextFrom($cssIdentifier);
+        $me->amOnPage('/test/error-handler/exception');
+        $errorIdSecondAccess = $me->grabTextFrom($cssIdentifier);
+        Assert::assertNotSame($errorIdFirstAccess, $errorIdSecondAccess);
     }
 }
